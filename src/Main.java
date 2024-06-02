bb486d16f1a4e066c83d1869e9fec78568388e90
Gustavo <gustavo.g.o.16@hotmail.com>
2024-06-02 15:30:24 -0300

    Implementação do pattern Observer em Java

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ConcreteSubject subject = new ConcreteSubject();
+
+        ConcreteObserver observador1 = new ConcreteObserver("Mickey Mouse");
+        ConcreteObserver observador2 = new ConcreteObserver("Pateta");
+        ConcreteObserver observador3 = new ConcreteObserver("Pato Donald");
+
+        subject.registerObserver(observador1);
+        subject.registerObserver(observador2);
+        subject.registerObserver(observador3);
+
+        System.out.println("Definindo mensagem a ser enviada para os observadores:");
+        subject.setMessage("Boa noite!");
+
+
+        System.out.println("\nRemovendo observador 2");
+        subject.removeObserver(observador2);
+
+
+        System.out.println("'Mudando a mensagem:'");
+        subject.setMessage("Dinossauro Rex");
+
+
+        System.out.println("\nRemovendo observador 1");
+        subject.removeObserver(observador1);
+
+        System.out.println("Definindo mensagem final:");
+        subject.setMessage("Chop Suey!");
+    }
+}
